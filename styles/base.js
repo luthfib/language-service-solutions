8b8458cd652fb2c2f1de4ea33dff4676926bee30
Sarmun Bustillo <sarmun.b@gmail.com>
2020-07-18 13:51:42 +0200

    Stlye folder and base.js file color definitions

---
+++ styles/base.js
@@ -0,0 +1,11 @@
+import css from 'styled-jsx/css';
+
+export default css.global`
+:root {
+    --primary-dark: #346278;
+    --primary-medium: #539b92;
+    --primary-light: #7fbba1;
+
+    --gradient: linear-gradient(to right,var(--primary-dark), var(--primary-medium),  var(--primary-light) );
+}
+`
