93935186433e64ed31bd11de7ca31662e2d573cc
Luthfi  bustillos <luthfibustillos@gmail.com>
2020-08-16 23:11:19 -0400

    add some stuff, gotta figure out some bugs

---
+++ components/Gallery.js
@@ -0,0 +1,19 @@
+const Gallery = () => (
+  <>
+    <div class="img-container">
+      <img src="/imgs/TeamGroup.jpg" />
+    </div>
+    <style jsx>{`
+        div {
+            width: 100%;
+            height: 50%;
+        }
+        img {
+            width: 100%;
+        }
+    `}
+    </style>
+  </>
+);
+
+export default Gallery;
