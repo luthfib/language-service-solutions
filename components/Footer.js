d5a7c8d73788af8459d6f9ae54dbff6cbce714ea
Sarmun Bustillo <sarmun.b@gmail.com>
2020-09-13 23:48:45 +0200

    Footer main structure

---
+++ components/Footer.js
@@ -0,0 +1,131 @@
+import { useRef } from 'react';
+import { useOnClickOutside } from '../hooks/useOnClickOutside';
+import Logo from './Icons/logo';
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='footer'>
+        <div className='inner-footer'>
+          <div className='logo-container'>
+            <a href='#' className='logo'>
+              <Logo>Logo</Logo>
+            </a>
+            <h4 className='lss'>
+              LANGUAGE <br /> SERVICE <br /> SOLUTIONS
+            </h4>
+          </div>
+          <div className='contact-container'>
+            <p>CONTACT US</p>
+            <small>Phone: (919) 949-9272</small>
+            <small>Email: murtado@languageservicesolutions</small>
+            <ul>
+              <li className='icon'>
+                <a className='facebook'></a>
+              </li>
+              <li className='icon'>
+                <a className='youtube'></a>
+              </li>
+              <li className='icon'>
+                <a className='twitter'></a>
+              </li>
+              <li className='icon'>
+                <a className='linkdin'></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+      <style jsx>
+        {`
+          .footer {
+            background-image: var(--gradient2);
+            width: 100%;
+            display: flex;
+            justify-content: center;
+          }
+          .inner-footer {
+            display: flex;
+            width: 100%;
+            height: 100%;
+            align-items: center;
+            justify-content: space-around;
+            flex-wrap: wrap;
+            padding: 25px 0 25px;
+            max-width: 700px;
+          }
+          .logo-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          .logo {
+            border: 1px solid var(--white);
+            border-radius: 50%;
+            width: 100px;
+            height: 100px;
+            margin-right: 10px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .lss {
+            font-size: 26px;
+            line-height: 26px;
+            font-weight: 100;
+            color: #353535;
+          }
+
+          .contact-container {
+            display: flex;
+            flex-direction: column;
+          }
+
+          ul {
+            display: flex;
+            flex-direction: row;
+            margin-left: 0;
+            padding-left: 0;
+            list-style: none;
+          }
+
+          li {
+            background-color: var(--white);
+            height: 40px;
+            width: 40px;
+            border-radius: 50%;
+            margin-right: 5px;
+          }
+
+          p {
+            font-size: 18px;
+          }
+
+          small {
+            color: #3b3b3b;
+          }
+          @media (hover: hover) {
+          }
+
+          @media (max-width: 550px) {
+            .inner-footer {
+              flex-direction: column;
+              align-items: center;
+              justify-content: center;
+            }
+            .contact-container {
+              text-align: center;
+            }
+
+            ul {
+              justify-content: center;
+            }
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default Footer;
