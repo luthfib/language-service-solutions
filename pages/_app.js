8ab320cd7e93e3f1bbde5121d2ff2fdb834211e2
Luthfi Bustillos <luthfi.programming@gmail.com>
2020-10-11 14:41:28 -0400

    remove that bad hombre

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
