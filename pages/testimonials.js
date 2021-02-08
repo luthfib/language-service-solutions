3b4f82fbc47d3195ea33f41d9bd8ed7adddb19c4
luthfib <luthfibustillos@gmail.com>
2021-02-07 20:15:56 -0500

    attempt to clear cache

--- pages/testimonials.js
+++ pages/testimonials.js
@@ -4,7 +4,7 @@ import Banner from '../components/Banner';
 import Dots from '../components/Icons/dots';
 import Footer from '../components/Footer';
 import Head from 'next/head';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar2';
 import Textbox from '../components/TextBox';
 import { useOnWindowScroll } from '../hooks/useOnWindowScroll';
 
