2b3ea79c102716902936b1176f4e4922668c4a6e
luthfib <luthfibustillos@gmail.com>
2021-02-07 20:33:14 -0500

    about us alignment

--- components/AboutUsSection.js
+++ components/AboutUsSection.js
@@ -1,32 +1,41 @@
 const AboutUs = (props) => (
   <>
-    <div>
-      <h2>about us</h2>
-      <p>
-        LANGUAGE SERVICE SOLUTIONS, LLC has been in the field of Spanish/English language services
-        since 2000. As a result of 15 years of solid experience, we have become one of the most
-        trusted full-service language providers in the Triangle
-      </p>
-      <p>
-        Headquartered in Durham, Language Service Solutions, LLC, is a consurtiom of certified,
-        professional interpreters and translators with extensive experience eliminating language
-        barriers betweeen businesses, nonprofits, and healthcare facilities--small and large--and
-        the clients they serve
-      </p>
-      <p>
-        Our professional, friendly, and experienced team works hard to provide our clients with the
-        best service. We are bi-cultural/bilingual group of professionals who understand the subtle
-        cultural differences that play a role in communication
-      </p>
+    <div className="about-holder">
+      <div className="about-us">
+        <h2>about us</h2>
+        <p>
+          LANGUAGE SERVICE SOLUTIONS, LLC has been in the field of
+          Spanish/English language services since 2000. As a result of 15 years
+          of solid experience, we have become one of the most trusted
+          full-service language providers in the Triangle
+        </p>
+        <p>
+          Headquartered in Durham, Language Service Solutions, LLC, is a
+          consurtiom of certified, professional interpreters and translators
+          with extensive experience eliminating language barriers betweeen
+          businesses, nonprofits, and healthcare facilities--small and
+          large--and the clients they serve
+        </p>
+        <p>
+          Our professional, friendly, and experienced team works hard to provide
+          our clients with the best service. We are bi-cultural/bilingual group
+          of professionals who understand the subtle cultural differences that
+          play a role in communication
+        </p>
+      </div>
     </div>
     <style jsx>{`
-      div {
+      .about-holder {
         background: var(--primary-light);
         border-radius: var(--border-radius);
         width: 90%;
         padding: 30px 80px ;
-        text-align: center;
         box-shadow: var(--elevation-3);
+      }
+
+      .about-us {
+        max-width: 500px;
+        margin: auto;
       }
 
       h2 {
@@ -36,7 +45,7 @@ const AboutUs = (props) => (
       }
 
       @media (max-width: 768px) {
-        div {
+        .about-holder {
           padding: 30px 40px;
           width: 100%;
         }
