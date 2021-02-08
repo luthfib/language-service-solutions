ed40cbdcd7f5ed09b32bd375aa2988e851015284
luthfib <luthfibustillos@gmail.com>
2021-02-07 21:27:22 -0500

    add a base style that allows one to use css var in one place and applies to all pages

--- pages/contact.js
+++ pages/contact.js
@@ -55,7 +55,7 @@ export default function Home() {
         <section className='margin-bottom-1'>
           <a href='mailto:murtado@languageservicesolutions' className='banner-wrapper'>
             <Banner
-              bgColor={'#0f2832'}
+              bgColor={'var(--dark-blue)'}
               height={'90px'}
               width={'90%'}
               headingLevel={'h2'}
@@ -126,59 +126,7 @@ export default function Home() {
 
         @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;800&display=swap');
         :root {
-          /* colors */
 
-          --primary-dark: #346278;
-          --primary-medium: #539b92;
-          --primary-light: #7fbba1;
-          --white: #fff;
-          --black: #000;
-          --transparent: #ffffff00;
-
-          --gradient: linear-gradient(
-            to right,
-            var(--primary-dark),
-            var(--primary-medium),
-            var(--primary-light)
-          );
-          --gradient2: linear-gradient(
-            to left,
-            var(--primary-dark),
-            var(--primary-medium),
-            var(--primary-light)
-          );
-
-          --gradient3: linear-gradient(
-            10deg,
-            var(--primary-light),
-            var(--primary-medium),
-            var(--primary-dark)
-          );
-
-          --nav-height: 100px;
-          /* Shadows Vercel */
-
-          --shadow-smallest: 0px 4px 8px rgba(0, 0, 0, 0.12);
-          --shadow-small: 0 5px 10px rgba(0, 0, 0, 0.12);
-          --shadow-medium: 0 8px 30px rgba(0, 0, 0, 0.12);
-          --shadow-large: 0 30px 60px rgba(0, 0, 0, 0.12);
-          --shadow-hover: 0 30px 60px rgba(0, 0, 0, 0.12);
-
-          /* Shadows level up tuts */
-
-          --elevation-0: inset 0 7px 9px -7px rgba(0, 0, 0, 0.7);
-          --elevation-1: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-          --elevation-2: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-          --elevation-3: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1);
-          --elevation-4: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-          --elevation-5: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
-
-          /* max-width content */
-          --content-width: 1400px;
-
-          /* border-radius */
-          --border-radius-mobile: 20px;
-          --border-radius: 20px;
         }
         html,
         body {
