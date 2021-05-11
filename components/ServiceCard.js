82137dc087c43aefedcb8faabdfc92002f10e364
luthfib <luthfibustillos@gmail.com>
2021-05-11 00:23:08 -0400

    fix accessibility concerns

--- components/ServiceCard.js
+++ components/ServiceCard.js
@@ -1,16 +1,16 @@
 import Link from "next/link";
 const ServiceCard = ({ logo, title, text }) => (
   <>
-  <Link href={`services/${logo}`}>
-    <a  id={`${logo}`} className='card-wrapper'>
-      <div className='card'>
-        <div className='icon '>
-          <img width='80px' height='80px' src={`/icons/${logo}.svg`} />
+    <Link href={`services/${logo}`}>
+      <a id={`${logo}`} className="card-wrapper">
+        <div className="card">
+          <div className="icon ">
+            <img width="80px" height="80px" src={`/icons/${logo}.svg`} alt={logo} />
+          </div>
+          <h4>{title}</h4>
+          <p>{text}</p>
         </div>
-        <h4>{title}</h4>
-        <p>{text}</p>
-      </div>
-    </a>
+      </a>
     </Link>
     <style jsx>{`
       .card-wrapper {
@@ -74,11 +74,12 @@ const ServiceCard = ({ logo, title, text }) => (
       .banner-wrapper:hover {
         background-color: var(--white);
       }
-      {/* FOR LAST CARD */}
-      #medicalInterpretation div{
+       {
+        /* FOR LAST CARD */
+      }
+      #medicalInterpretation div {
         margin-bottom: 0;
       }
-
     `}</style>
 
     <style jsx global>
@@ -91,8 +92,6 @@ const ServiceCard = ({ logo, title, text }) => (
           color: var(--primary-medium);
           transition: color 0.3s ease;
         }
-
-       
 
         @media (max-width: 768px) {
           :root {
