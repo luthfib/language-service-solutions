a3db16c554edea3205ab0dd3b692ca1890efae7e
luthfib <luthfibustillos@gmail.com>
2021-08-16 18:01:31 -0400

    adjust site to be under maintenance

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,30 @@
 import App from "next/app";
+import ComingSoon from "../components/ComingSoon";
 import Layout from "../components/Layout";
 import baseTheme from "../styles/base";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-
-    return ( 
-        <Layout>
-          <Component {...pageProps} />
-          <style jsx global>
-            {baseTheme}
-          </style>
-        </Layout>
+    const underMaintenance = true;
+    return (
+      <>
+        {!underMaintenance ? (
+          <Layout>
+            <Component {...pageProps} />
+            <style jsx global>
+              {baseTheme}
+            </style>
+          </Layout>
+        ) : (
+          <>
+            <style jsx global>
+              {baseTheme}
+            </style>
+            <ComingSoon />
+          </>
+        )}
+      </>
     );
   }
 }
