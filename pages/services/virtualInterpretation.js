f9a3177dacf093ced46adb0b3f58db960d75dab9
luthfib <luthfibustillos@gmail.com>
2021-11-28 15:51:15 -0500

    more fixes to virtual intepretation

--- pages/services/virtualInterpretation.js
+++ pages/services/virtualInterpretation.js
@@ -62,9 +62,7 @@ export default function Home() {
       />
       <section className="margin-bottom-1">
         <ServiceSingle
-        background="var(--wheat)"
-       
-          // logo={"telephonicInterpretation"}
+          background="var(--wheat)"
           innerText={
             <>
               <p className="no-margin-top font-weight-small">
@@ -121,6 +119,81 @@ export default function Home() {
                 We’ll be happy to answer any questions you may have and give you
                 a free estimate.
               </p>
+            </>
+          }
+        />
+      </section>
+      <Banner
+        bgColor={""}
+        height={"90px"}
+        width={"90%"}
+        headingLevel={"h1"}
+        color={"var(--green-darker)"}
+        margin={"50"}
+        text={"Telephonic Interpretation"}
+        dots={false}
+      />
+      <section className="margin-bottom-1">
+        <ServiceSingle
+          // logo={"telephonicInterpretation"}
+          innerText={
+            <>
+              <p className="no-margin-top">
+                Language Service Solutions is proud to provide your organization
+                with the capacity to reach your Spanish-speaking clients
+                seamlessly and without delay by offering our telephonic
+                interpretation service.
+              </p>
+              <br />
+              <h4 className="headline text-white no-margin-top small-margin-bottom upperCase ">
+                WHAT IS TELEPHONIC INTERPRETATION?
+              </h4>
+              <p className="no-margin-top">
+                This type of interpretation refers to, as it sounds,
+                interpretation done over the phone. You simply call us and the
+                interpreter will walk you through the simple process. The main
+                difference to all other types of interpretation is that since it
+                is done consecutively over the phone it reduces the need for an
+                interpreter to be physically present at the time of the
+                appointment. Saving you time and money.
+              </p>
+              <br />
+
+              <h4 className="headline text-white no-margin-top small-margin-bottom upperCase ">
+                EXAMPLES OF COMMON USES
+              </h4>
+              <p className="no-margin-top">
+                Family therapies, one-on-one meetings or sessions, follow-ups,
+                interviews, house visits, conference calls with clients,
+                parent-teacher conferences.
+              </p>
+              <br />
+
+              <h4 className="headline text-white no-margin-top small-margin-bottom upperCase ">
+                DEPENDABLE. CONFIDENTIAL. AFFORDABLE.
+              </h4>
+              <p className="no-margin-top">
+                Our team of professional interpreters will guarantee you have a
+                confidential and culturally appropriate experience throughout
+                the process. Based in the Triangle, we are familiar with the
+                locations, organizations and the community, giving us the local
+                awareness and knowledge to guarantee provide high quality
+                service that is relevant to your needs. Additionally, we can
+                offer to dispatch an interpreter to meet you wherever you are,
+                should your needs change—something many other companies cannot
+                offer.
+              </p>
+              <br />
+              <h4 className="headline text-white no-margin-top small-margin-bottom upperCase ">
+                QUESTIONS OR CONCERNS?
+              </h4>
+              <p className="no-margin-top">
+                Still not sure if this service is right for you? Call us today
+                for your free consultation and we’ll address any
+                questions/concerns you have and help you find the best solution
+                for your needs.
+              </p>
+              <br />
             </>
           }
         />
