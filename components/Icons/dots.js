d150f202ed3b2dc613ea12e45698b335998e29de
Sarmun Bustillo <sarmun.b@gmail.com>
2023-04-10 15:16:17 +0700

    Changed dots to svg

--- components/Icons/dots.js
+++ components/Icons/dots.js
@@ -1,65 +1,57 @@
 // The dots can go in all 4 directions and its size can also be dinamically set through props
-const Dots = ({ direction, size, color = "var(--yellow)" }) => {
-  return (
-    <>
-      <div className={`dots ${direction}`}>
-        <span className="dot"></span>
-        <span className="dot medium"></span>
-        <span className="dot small"></span>
-      </div>
-      <style jsx>
-        {`
-          .dots {
-            --size: ${size}px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }
-          .dots.up {
-            flex-flow: column;
-          }
-          .dots.down {
-            flex-flow: column-reverse;
-          }
-          .dots.right {
-            flex-flow: row-reverse;
-          }
-          .dots.left {
-            flex-flow: row;
-          }
-
-          .dot {
-            height: var(--size);
-            width:  var(--size);
-            background: ${color};
-            position: relative;
-            border-radius: 50%;
-          }
-          .dot.medium {
-            height:  calc(var(--size) - (var(--size) / 5));
-            width:  calc(var(--size) - (var(--size) / 5));
-          }
-          .dot.small {
-            height:  calc(var(--size) - (var(--size) / 2));
-            width:  calc(var(--size)  - (var(--size) / 2));
-          }
-
-          .up .dot:not(:first-of-type) {
-            margin-top:  calc(var(--size) /7);
-          }
-          .down .dot:not(:last-of-type) {
-            margin-top:  calc(var(--size) /7);
-          }
-          .right .dot:not(:first-of-type) {
-            margin-right:  calc(var(--size) /7);
-          } 
-          .left .dot:not(:last-of-type) {
-            margin-right:  calc(var(--size) /7);
-          } 
-        `}
-      </style>
-    </>
-  );
+const Dots = ({ direction, size, color = 'var(--yellow)' }) => {
+    return (
+        <>
+            <div className={`dots ${direction}`}>
+                <svg
+                    id="Layer_2"
+                    data-name="Layer 2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 34.59 86.37"
+                    height={size * 3}
+                >
+                    <g id="Layer_1-2" data-name="Layer 1">
+                        <g>
+                            <circle
+                                class="cls-1"
+                                cx="17.3"
+                                cy="17.3"
+                                r="17.3"
+                                fill={color}
+                            />
+                            <circle
+                                class="cls-1"
+                                cx="17.3"
+                                cy="52.51"
+                                r="12.74"
+                                fill={color}
+                            />
+                            <circle
+                                class="cls-1"
+                                cx="17.3"
+                                cy="78.12"
+                                r="8.26"
+                                fill={color}
+                            />
+                        </g>
+                    </g>
+                </svg>
+            </div>
+            <style jsx>
+                {`
+                    .dots.down {
+                        transform: rotate(180deg);
+                    }
+                    .dots.right {
+                        transform: rotate(90deg);
+                    }
+                    .dots.left {
+                        transform: rotate(-90deg);
+                    }
+                `}
+            </style>
+        </>
+    );
 };
 
 export default Dots;
